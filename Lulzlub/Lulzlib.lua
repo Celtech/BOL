4e9916849cdb53a9cd8da462b67494957adfa1d4
Tim Hinz <timhinz16@gmail.com>
2017-01-15 11:31:58 -0600

    Create Lulzlib.lua

---
+++ Lulzlub/Lulzlib.lua
@@ -0,0 +1,8 @@
+class "Lulzlib"
+function Lulzlib:__init()
+    self.version = .01
+end
+
+function Log(message)
+    print("<font color='#5500AA'>["..myHero.charName.."]</font> <font color='#FFFFFF'>"..message.."</font>")
+end
